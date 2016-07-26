ac44362efe1b1b7b6ee741f89b1e83e632c17ec6
huangyong <hy873872182@gmail.com>
2016-07-26 15:55:10 +0800

    cilpboard

--- RNAppNavigator/index.android.js
+++ RNAppNavigator/index.android.js
@@ -3,50 +3,106 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
-import React, { Component } from 'react';
+'use strict';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TouchableHighlight,
+    Navigator
 } from 'react-native';
 
-class RNAppNavigator extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+class NavButton extends Component {
+    render() {
+        return (
+            <TouchableHighlight
+                style={styles.button}
+                underlayColor="#b5b5b5"
+                onPress={this.props.onPress}>
+                <Text style={styles.buttonText}>{this.props.text}</Text>
+            </TouchableHighlight>
+        );
+    }
+}
+
+class NavMenu extends Component{
+    render() {
+        return(
+            <View style={styles.scene}>
+                <Text style={styles.messageText}>{this.props.message}</Text>
+                <NavButton
+                    onPress={() => {
+                        this.props.navigator.push({
+                            message: '向右拖拽关闭页面',
+                            sceneConfig: Navigator.SceneConfigs.FloatFromRight
+                        });
+                    }}
+                    text = '从右边向左切入页面(带有透明度变化)'
+                />
+                <NavButton
+                    onPress={() => {
+                        this.props.navigator.push({
+                            message: '向下拖拽关闭页面',
+                            sceneConfig: Navigator.SceneConfigs.FloatFromBottom,
+                        });
+                    }}
+                    text="从下往上切入页面(带有透明度变化)"
+                />
+                <NavButton
+                    onPress={() => {
+                        this.props.navigator.popToTop();
+                    }}
+                    text = '页面弹出(回退到最后一页)'
+                />
+                <NavButton
+                    onPress={() => {
+                        this.props.navigator.pop();
+                    }}
+                    text = '页面弹出(回退一页)'
+                />
+            </View>
+        );
+    }
+}
+
+class RNAppNavigator extends Component{
+    render() {
+        return (
+            <Navigator
+                style={styles.container}
+                initialRoute= {{message: '页面初始化'}}
+                renderScene= {(route, navigator) => <NavMenu
+                    message = {route.message} navigator = {navigator}/>}
+                configureScene={(route) => {
+                    if (route.sceneConfig) {
+                        return route.sceneConfig;
+                    }
+                    return Navigator.SceneConfigs.FloatFromBottom;
+                }}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1
+    },
+    messageText: {
+        fontSize: 17,
+        fontWeight: '500',
+        padding: 15,
+        marginTop: 50,
+        marginLeft: 15
+    },
+    button: {
+        backgroundColor: 'white',
+        padding: 15,
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderBottomColor: '#cdcdcd'
+    }
 });
 
 AppRegistry.registerComponent('RNAppNavigator', () => RNAppNavigator);
